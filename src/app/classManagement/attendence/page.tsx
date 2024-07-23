f029981cecf941b5257ec8125b517297f43d31a9
SiddhantXelp <siddhant.kumar@xelpmoc.in>
2024-07-23 11:49:53 +0530

    feat: class management table

--- src/app/classManagement/attendence/page.tsx
+++ src/app/classManagement/attendence/page.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
+import { RecentClassesColumns, RecentClassesData, StudentColumns, StudentsData } from './data';
+import Table from '@/components/table';
 
 const Attendance: React.FC = () => {
+
+  const [selectedOption, setSelectedOption] = useState('option1');
+
+  const handleOptionChange = (option: string) => {
+    setSelectedOption(option);
+  };
+
+  const getTableData = () => {
+    if (selectedOption === 'option1') {
+      return { columns: RecentClassesColumns, data: RecentClassesData };
+    }
+    return { columns: StudentColumns, data: StudentsData };
+  };
+
+  const { columns, data } = getTableData();
   return (
     <>
       <div className="flex flex-wrap space-x-5">
@@ -69,6 +88,31 @@ const Attendance: React.FC = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="p-4">
+        <div className="flex space-x-4 mb-4">
+          <span
+            onClick={() => handleOptionChange('option1')}
+            className={`text-sm cursor-pointer mr-10 text-xxs ${selectedOption === 'option1' ? 'font-bold' : ''}`}
+          >
+           Recent Classes
+          </span>
+
+          {/* <span
+          className={`text-sm cursor-pointer text-xxs ${selectedOption === 'Overview' ? 'font-bold' : ''}`}
+          onClick={() => setSelectedTab('Overview')}
+        >
+          Overview
+        </span> */}
+
+          <span
+            onClick={() => handleOptionChange('option2')}
+            className={`text-sm cursor-pointer text-xxs ${selectedOption === 'option2' ? 'font-bold' : ''}`}
+          >
+            Students
+          </span>
+        </div>
+        <Table columns={columns} data={data} includeCheckbox={false} />
       </div>
     </>
   );
