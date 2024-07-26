4aa873c49da565639a585907af32c38973d76940
gopisunkaraxelp <gopi.sunkara@xelpmoc.in>
2024-07-26 09:37:41 +0530

    new Changes responsive

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -113,19 +113,19 @@ const StudentPage: React.FC = () => {
         </Link>
       </div>
 
-      <div className='flex flex-row sm:gap-2 md:gap-4 lg:gap-6 mt-10 '>
+      <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-5  items-center  mt-5'>
         {data.map((item, index) => (
           <div className={(index === 0 || index === 2) ? 'cursor-pointer' : ''} key={index} onClick={() => (index === 0 || index === 2) && handleClick(index)}>
             <div className='flex flex-row justify-between'>
               <span className='text-buttonGray text-xxxs'>{item.cardName}</span>
-              <span className='text-buttonGray mt-1'>
+              <span className='text-buttonGray mr-5'>
                 <p className='text-xxxs'>
                   {item.cardNameEnd}
                 </p>
               </span>
             </div>
             <div className='flex flex-wrap gap-4 items-center '>
-              <div className='md:w-32 lg:w-38 xl:w-56 2xl:w-48 h-30 bg-buttonGray border rounded-md flex flex-col justify-between p-4'>
+              <div className='sm:w-24 md:w-32 lg:w-38 xl:w-56 2xl:w-48 h-30 bg-buttonGray border rounded-md flex flex-col justify-between p-4'>
                 <div>
                   <h1 className='text-white text-xs font-bold'>{item.subject}</h1>
                   {item.details.map((detail, detailIndex) => (
@@ -137,7 +137,7 @@ const StudentPage: React.FC = () => {
           </div>
         ))}
 
-        <div className='w-32 h-30 bg-buttonGray border rounded-md mt-6 flex items-center justify-center'>
+        <div className='md:w-28  lg:w-32 xl:w-40 2xl:w-44 h-20 bg-buttonGray border rounded-md mt-2 flex items-center justify-center'>
           <div className='flex flex-col items-center'>
             <GrAdd color='white' size={"20px"} />
             <p className='text-white text-xxs'>Add Student</p>
