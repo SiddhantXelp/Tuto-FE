6e61165c739a26482b3f9a35083537d4c8f1ea97
gopisunkaraxelp <gopi.sunkara@xelpmoc.in>
2024-08-01 18:19:57 +0530

    new changes on the  stepper in dashbord

--- src/app/api/classes.ts
+++ src/app/api/classes.ts
@@ -1,8 +1,7 @@
 import {BASE_URL} from './api';
 
 export default {
-//   getClasses: `${BASE_URL}/api/v1/classes`,
+  getClasses: `${BASE_URL}/api/v1/classes`,
 
-getClasses: `https://jsonplaceholder.typicode.com/posts`,
   
 };
