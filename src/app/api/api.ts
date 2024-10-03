016a340f1a0c50ee9e9f24be5b8d8b1b79cbc625
SiddhantXelp <siddhant.kumar@xelpmoc.in>
2024-10-03 14:49:54 +0530

    feat: env and merge

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -2,7 +2,7 @@
 
 // export const BASE_URL = process.env.DEV_BASE_URL;
 
-export const BASE_URL = 'http://localhost:6800';
+// export const BASE_URL = 'http://localhost:6800';
 
 
-// export const BASE_URL = 'http://tutor.xelpmoc.in:6800';
+export const BASE_URL = 'http://tutor.xelpmoc.in:6800';
